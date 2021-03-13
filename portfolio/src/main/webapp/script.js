f19cf1d91d2d5915a55b4996903b64ee1c9a6d0c
Norik <norikzhagui@gmail.com>
2021-03-13 19:27:33 +0000

    Add a function to initalize the map object

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,3 +31,8 @@ function addRandomMessage(list) {
   const messageContainer = document.getElementById('message-container');
   messageContainer.innerText = random_text;
 }
+function createMap(){
+    const map = new google.maps.Map(
+        document.getElementById("map"),
+        {center:{lat: 37.422, lng: -122.084}, zoom: 16});
+}
